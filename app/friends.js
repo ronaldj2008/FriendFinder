5f3180b1eb7e0893a2929c16fe0630404c29d41a
Ron J <ronaldj2008@gmail.com>
2017-05-02 17:25:10 -0700

    server logic. need html only

---
+++ app/friends.js
@@ -0,0 +1,178 @@
+
+var friendsArray = [
+	{
+		name: 'Bria'
+		photo: 'https://goo.gl/images/PMmTrl',
+		scores: [
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+
+		]
+	},
+	{
+		name: 'Mya'
+		photo: 'https://goo.gl/images/dIFUww',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4'
+
+		]
+	},
+	{
+		name: 'Kehlani',
+		photo: 'https://goo.gl/images/WVnh8h',
+		scores: [
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3'
+
+		]
+	},	
+	{
+		name: 'Jhene',
+		photo: 'https://goo.gl/images/pXVlkE',
+		scores: [
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2'
+		]
+	},
+		{
+		name: 'Sza',
+		photo: 'https://goo.gl/images/LfrAZx',
+		scores: [
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4'
+		]
+	},
+		{
+		name: 'Joyce',
+		photo: 'https://goo.gl/images/f3bvYq',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+		{
+		name: 'Quavo',
+		photo: 'https://goo.gl/images/sF18YD',
+		scores: [
+			'1',
+			'2',
+			'3',
+			'4',
+			'5',
+			'1',
+			'2',
+			'3',
+			'4',
+			'5'
+		]
+	},
+		{
+		name: 'Demar',
+		photo: 'https://goo.gl/images/8O2oQX',
+		scores: [
+			'1',
+			'2',
+			'1',
+			'2',
+			'1',
+			'2',
+			'1',
+			'2',
+			'1',
+			'2'
+		]
+	},
+		{
+		name: 'Lance',
+		photo: 'https://goo.gl/images/xu5xvP',
+		scores: [
+			'2',
+			'3',
+			'2',
+			'3',
+			'2',
+			'3',
+			'2',
+			'3',
+			'2',
+			'3'
+		]
+	},
+		{
+		name: 'Iman',
+		photo: 'https://goo.gl/images/Un3fAi',
+		scores: [
+			'3',
+			'4',
+			'3',
+			'4',
+			'3',
+			'4',
+			'3',
+			'4',
+			'3',
+			'4'
+		]
+	}
+];
+
+module.exports = friendsArray;
+
+
+
+
+
+
+
+
+
+
